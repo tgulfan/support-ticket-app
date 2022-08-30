ca24d7d2cdf611b78b01b9813b53c21601d46142
tgulfan <tgulfan@gmail.com>
2022-08-30 23:45:16 +1000

    header and initial pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route element={<Home />} exact path='/' />
+            <Route element={<Login />} path='/login' />
+            <Route element={<Register />} path='/register' />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
