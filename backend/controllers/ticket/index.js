ee344807c7ec8f3570ad73266cde56837ee1b06d
tgulfan <tgulfan@gmail.com>
2022-09-20 00:36:20 +1000

    add get single ticket route

--- backend/controllers/ticket/index.js
+++ backend/controllers/ticket/index.js
@@ -5,7 +5,6 @@ const Ticket = require('../../models/ticket');
 
 // Get user tickets
 // /api/tickets
-
 const getTickets = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
 
@@ -18,6 +17,33 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// Get specific user ticket
+// /api/tickets/:id
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
+// Create new ticket
+// POST /api/tickets
 const createTickets = asyncHandler(async (req, res) => {
   const { product, description } = req.body;
 
@@ -43,4 +69,4 @@ const createTickets = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTickets };
+module.exports = { getTicket, getTickets, createTickets };
--- backend/routes/ticket/index.js
+++ backend/routes/ticket/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTickets, getTickets } = require('../../controllers/ticket');
+const {
+  createTickets,
+  getTicket,
+  getTickets,
+} = require('../../controllers/ticket');
 const { protect } = require('../../middleware/auth');
 
 router.route('/').get(protect, getTickets).post(protect, createTickets);
+router.route('/:id').get(protect, getTicket);
 
 module.exports = router;
